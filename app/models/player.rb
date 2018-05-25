4751e248029fdb0eeea24ce4c0b1eb25b7d73b7a
sdrudz <pedropauloroachy@gmail.com>
2018-05-24 21:12:26 -0300

    controllers_models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,9 @@
-class Player < ApplicationRecord
+class​ ​ Player​ < ApplicationRecord
+belongs_to ​ :movie
+belongs_to ​ :user
+before_create ​ :set_start_date
+private
+def​ ​ set_start_date
+self​ .start_date ||= Time.zone.now
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :trackable, :validatable
+  has_many :reviews
+  has_many :favorites
+  has_many :players
+  validates :name, presence: true, on: :update
 end
